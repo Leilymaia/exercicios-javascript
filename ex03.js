de71b346bfb1a83bf0ac2699c9869cdf3fdafa12
Leiliane Maia <leilianemaia03@gmail.com>
2022-03-31 19:15:57 -0300

    novo exercicio
    atualizando pasta

---
+++ ex03.js
@@ -0,0 +1,9 @@
+var vel = 80
+
+console.log(` A velocidade do seu carro é ${vel}km/h `)
+if (vel > 60) {
+console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
+
+
